6cac0be359b8c97e3a816e62bf81fd393a799fb8
Jeff Lee <somejeff@gmail.com>
2024-04-04 11:13:57 -0400

    SImple Terminal and Gate Assignments

--- src/VatFib.js
+++ src/VatFib.js
@@ -1,28 +1,125 @@
+/**
+ * Flight information board: Displays inbound and outbound flights with gate assignments and status
+ *
+ */
 class VatFib {
-    constructor (config) {
-        this.config = {
-            gateAssignments: [
-                {
-                    terminal: "Main"
-                }
-            ]
-        }
-        this.flights = {
-            arrivals: [],
-            departures: []
-        }
+  constructor(config) {
+    // Default configuration
+    this.config = {
+      // Defaults to a main terminal
+      terminals: [
+        {
+          // callsign: any
+          // aircraft: any
+          name: "Main",
+          gates: [
+            {
+              // callsign: any
+              // aircraft: any
+              name: "A",
+            },
+          ],
+        },
+      ],
+    };
+    // Merge user-provided configurations with default configurations
+    if (config && typeof config === "object") {
+      this.config = { ...this.config, ...config };
     }
 
-    setFlights(flights) {
-        this.flights = flights
-        this.assignGates()
-    }
+    //
+    this.flights = {
+      arrivals: [],
+      departures: [],
+    };
+  }
 
-    assignGates() {
-        for(let i=0;i<this.flights.arrivals.length;i++) {
-            const flight = this.flights.arrivals[i];
-            flight.terminal = this.config.gateAssignments[0].terminal
-        }
+  setFlights(flights) {
+    this.flights = flights;
+    this.assignTerminalAndGates();
+  }
+
+  assignTerminalAndGates() {
+    this.flights.departures?.forEach((flight) => {
+      const selectedTerminal = this.selectTerminal(flight);
+      const selectedGate = selectedTerminal.gates[0];
+      flight.terminal = selectedTerminal.name;
+      flight.gate = selectedGate.name;
+    });
+
+    this.flights.arrivals?.forEach((flight) => {
+      const selectedTerminal = this.selectTerminal(flight);
+      const selectedGate = this.selectGate(flight,selectedTerminal);
+      flight.terminal = selectedTerminal.name;
+      flight.gate = selectedGate.name;
+    });
+  }
+
+  /**
+   * Selects an appropriate terminal for the given flight
+   *
+   * There may be constraints (e.g. callsign prefix or aircraft types)
+   *
+   * @param flight
+   * @returns a reproducible terminal with a gate list
+   */
+  selectTerminal(flight) {
+    // filter the terminal based on constraints of the flight
+    const availableTerminals = this.config.terminals.filter((terminal) => {
+      return true;
+    });
+    // only 1 terminal?
+    if (availableTerminals.length == 1) {
+      return availableTerminals[0];
     }
+    // multiple choices, key the flight and pick one
+    const flightHash = this.calculateHashNumber(flight);
+    return availableTerminals[flightHash % availableTerminals.length];
+  }
+
+  
+  /**
+   * Selects an appropriate gate from the passed in terminal for the given flight
+   *
+   * There may be constraints (e.g. callsign prefix or aircraft types)
+   *
+   * @param flight
+   * @param terminal containing gates
+   * @returns a reproducible gate
+   */
+  selectGate(flight, terminal) {
+    // filter the gate based on constraints of the flight
+    const availableGates = terminal.gates.filter((terminal) => {
+      return true;
+    });
+    // only 1 terminal?
+    if (availableGates.length == 1) {
+      return availableGates[0];
+    }
+    // multiple choices, key/reuse the flight and pick one
+    const flightHash = this.calculateHashNumber(flight);
+    return availableGates[flightHash % availableGates.length];
+  }
+
+  /**
+   * Calculates a reproducible has number based on the characteristics of the flight
+   */
+  calculateHashNumber(flight) {
+    // already calculated
+    if (flight.hash) {
+      return flight.hash;
+    }
+    // Concatenate the data points into a single string
+    const inputString = `${flight.cid}_${flight.callsign}_${flight.departure}_${flight.departure}_${flight.deptime}_${flight.arrtime}`;
+
+    // Convert the input string into a numerical value (simple hash)
+    let hash = 0;
+    for (let i = 0; i < inputString.length; i++) {
+      hash = (hash << 5) - hash + inputString.charCodeAt(i);
+      hash |= 0; // Convert to 32-bit integer
+    }
+    flight.hash = Math.abs(hash) // Ensure non-negative value and cache for reuse
+    return flight.hash; 
+  }
 }
 module.exports = VatFib;
---
+++ src/__tests__/VatFib_SimpleTerminalAndGates.js
@@ -0,0 +1,524 @@
+const VatFib = require("../VatFib");
+
+describe("Default Terminal and Gate", () => {
+  const fib = new VatFib();
+  it("should initialize a fib", () => {
+    expect(fib).not.toBeNull();
+    expect(fib.flights.arrivals.length).toBe(0);
+    expect(fib.flights.departures.length).toBe(0);
+  });
+
+  it("should accept a basic departure flight and assign the main terminal", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "CF-KMT",
+        },
+      ],
+    });
+
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+  });
+  it("should accept a basic arrival flight and assign the main terminal", () => {
+    fib.setFlights({
+      arrivals: [
+        {
+          callsign: "CF-KMT",
+        },
+      ],
+    });
+
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+  });
+
+  it("should accept departure and arrival flights and assign the main terminal", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "CF-KMT",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "CF-KMT",
+        },
+      ],
+    });
+
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+  });
+
+  it("should accept a multiple flights and assign the main terminal", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "CF-KMT",
+        },
+        {
+          callsign: "ACA1234",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "CF-KMT",
+        },
+        {
+          callsign: "ACA1234",
+        },
+      ],
+    });
+
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toBe("Main");
+      expect(flight.gate).toMatch("A");
+    });
+  });
+});
+
+describe("Custom Terminal and Gate", () => {
+  const fib = new VatFib({
+    terminals: [
+      {
+        name: "1",
+        gates: [
+          {
+            name: "B",
+          },
+        ],
+      },
+    ],
+  });
+
+  it("should assign the custom terminal and gate", () => {
+    fib.setFlights({
+      arrivals: [
+        {
+          callsign: "CF-KMT",
+        },
+      ],
+    });
+
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toBe("1");
+      expect(flight.gate).toMatch("B");
+    });
+  });
+});
+
+
+describe("Multiple terminals with a default gate", () => {
+  const fib = new VatFib({
+    terminals: [
+      {
+        name: "T1",
+        gates: [
+          {
+            name: "A",
+          },
+        ],
+      },
+      {
+        name: "T2",
+        gates: [
+          {
+            name: "B",
+          },
+        ],
+      },
+    ],
+  });
+
+  it("should assign the custom terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+
+    // All flights gate A or B
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toMatch(/T1|T2/);
+      expect(flight.gate).toMatch(/[AB]/); 
+    });
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toMatch(/T1|T2/);
+      expect(flight.gate).toMatch(/[AB]/);
+    });
+
+  });
+
+  it("should reproduce the same terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+    /**
+     * Reproducibe results
+     */
+
+    // capture the data from the firstRun
+    const firstRun = fib.flights;
+    // set the flights again, but with 1 extra pilot
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+    });
+    // remove the last flight
+    fib.flights.departures.pop()
+    fib.flights.arrivals.pop()
+    // the first flights should have not changed terminals/gates
+    expect(fib.flights).toMatchObject(firstRun);
+
+    
+
+  });
+});
+
+
+describe("Multiple Gates at the main terminal", () => {
+  const fib = new VatFib({
+    terminals: [
+      {
+        name: "T1",
+        gates: [
+          {
+            name: "A",
+          },
+          {
+            name: "B",
+          },
+        ],
+      },
+    ],
+  });
+
+  it("should assign the custom terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+
+    // All flights gate A or B
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toBe("T1");
+      expect(flight.gate).toMatch(/[AB]/); 
+    });
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toBe("T1");
+      expect(flight.gate).toMatch(/[AB]/);
+    });
+
+    
+  });
+
+  it("should reproduce the same terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+    /**
+     * Reproducibe results
+     */
+
+    // capture the data from the firstRun
+    const firstRun = fib.flights;
+    // set the flights again, but with 1 extra pilot
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+    });
+    // remove the last flight
+    fib.flights.departures.pop()
+    fib.flights.arrivals.pop()
+    // the first flights should have not changed terminals/gates
+    expect(fib.flights).toMatchObject(firstRun);
+  });
+});
+
+
+
+
+describe("Multiple Gates at multiple terminals", () => {
+  const fib = new VatFib({
+    terminals: [
+      {
+        name: "T1",
+        gates: [
+          {
+            name: "A",
+          },
+          {
+            name: "B",
+          },
+        ],
+      },
+      {
+        name: "T2",
+        gates: [
+          {
+            name: "C",
+          },
+          {
+            name: "D",
+          },
+        ],
+      },
+    ],
+  });
+
+  it("should assign the custom terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+
+    // All flights gate A or B
+    fib.flights.departures.forEach((flight) => {
+      expect(flight.terminal).toMatch(/T1|T2/); 
+      expect(flight.gate).toMatch(/[ABCD]/); 
+    });
+    fib.flights.arrivals.forEach((flight) => {
+      expect(flight.terminal).toMatch(/T1|T2/); 
+      expect(flight.gate).toMatch(/[ABCD]/);
+    });
+
+    
+  });
+
+  it("should reproduce the same terminal and gate", () => {
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+      ],
+    });
+    /**
+     * Reproducibe results
+     */
+
+    // capture the data from the firstRun
+    const firstRun = fib.flights;
+    // set the flights again, but with 1 extra pilot
+    fib.setFlights({
+      departures: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+      arrivals: [
+        {
+          callsign: "F1",
+        },
+        {
+          callsign: "F2",
+        },
+        {
+          callsign: "F3",
+        },
+        {
+          callsign: "F4",
+        },
+      ],
+    });
+    // remove the last flight
+    fib.flights.departures.pop()
+    fib.flights.arrivals.pop()
+    // the first flights should have not changed terminals/gates
+    expect(fib.flights).toMatchObject(firstRun);
+  });
+});
